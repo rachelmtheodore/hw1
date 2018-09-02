22c58619c92a4584225eb207077c5e94049f22ae
rachelmtheodore <r.theodore@gmail.com>
2018-09-02 14:55:25 -0400

    Fix typos in comments.

--- scripts/make_times.sh
+++ scripts/make_times.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check number of lines in stimuli.log
+# Check number of lines in stimuli.log.
 echo "Lines in stimuli.log:"
 wc -l stimuli.log
 
@@ -12,13 +12,14 @@ Condition=$(awk -F' ' '{ print $2 }' stimuli.log | sort | uniq)
 echo "These values were identified for the variable Condition:"
 echo $Condition
 
-# For each value of variable Condition, identify matching rows in stimuli.log; for these rows, pass the first vector of stimuli.log to a file named by Condition value with .1d suffix.
+# For each value of variable Condition, identify matching rows in stimuli.log.
+# For these rows, pass the first vector of stimuli.log to a file named by Condition value with .1d suffix.
 # The -i option is not indicated for grep; search will be case-sensitive.
 for condition in $Condition
 	do
 		cat stimuli.log | grep $condition | awk -F ' ' '{print $1}' > data/$condition.1d
 	done
 
-# Report in terminal the number of lines in each file of subdirectory; confirm files were created.
+# Report in terminal the number of lines in each file of subdirectory data/; confirm files were created.
 echo "Lines for each file in data/:"
 wc -l data/*
